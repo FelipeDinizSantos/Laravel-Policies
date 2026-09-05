8d9e50da410e41484a7938d9d144ee0613796b14
Felipe Diniz <felipedinizdossantos0@gmail.com>
2026-09-05 19:59:04 -0300

    feat: prepara base de dados

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ['admin', 'user', 'visitor'];
+        $posts = [];
+
+        $user_id = 1;
+
+        foreach($roles as $role) {
+            $posts[] = [
+                'user_id' => $user_id++,
+                'title' =>  'Post do usuário: ' . $role,
+                'content' => 'Sem conteúdo (' . $role . ')',
+                'created_at' => now(),
+                'updated_at' => now()
+            ];
+        }
+
+        DB::table('posts')->insert($posts);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ['admin', 'user', 'visitor'];
+
+        $users = [];
+
+        foreach ($roles as $role) {
+            $users[] = [
+                'name' => $role,
+                'email' => $role . '@gmail.com',
+                'password' => bcrypt('123'),
+                'role' => $role,
+                'created_at' => now(),
+                'updated_at' => now()
+            ];
+        }
+
+        DB::table('users')->insert($users);
+    }
+}
